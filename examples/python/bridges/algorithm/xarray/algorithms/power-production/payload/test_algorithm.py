1a0522c27b91f442f8c39365f107e2ab7c1e8f40
Fabian Emmerich <fabian.emmerich@4-cast.de>
2021-10-21 22:02:49 +0200

    First draft of climetlab and xarray bridge

---
+++ examples/python/bridges/algorithm/xarray/algorithms/power-production/payload/test_algorithm.py
@@ -0,0 +1,13 @@
+import algorithm
+import mantik.types
+
+
+def test_apply():
+    bundle = mantik.types.Bundle(value=[[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    meta = mantik.types.MetaVariables()
+    expected = [[6], [15], [24]]
+
+    result = algorithm.apply(bundle, meta)
+
+    assert isinstance(result, mantik.types.Bundle)
+    assert result.value == expected
---
+++ examples/python/bridges/dataset/climetlab/datasets/power-production/payload/test_dataset.py
@@ -0,0 +1,12 @@
+import dataset
+import mantik.types
+
+
+def test_apply():
+    meta = mantik.types.MetaVariables()
+    expected = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+    result = dataset.get(meta)
+
+    assert isinstance(result, mantik.types.Bundle)
+    assert result.value == expected
